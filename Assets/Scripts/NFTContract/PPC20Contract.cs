3feb692c00499c1f398c34e31643254253b1bc54
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-08-01 18:02:06 +0900

    SweetPDex

---
+++ Assets/Scripts/NFTContract/PPC20Contract.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Nethereum.Web3;
+using Nethereum.ABI.FunctionEncoding;
+using Nethereum.Contracts;
+using System.Diagnostics.Contracts;
+
+public class PPC20 : MonoBehaviour
+{
+
+    //@notion 스마트 컨트랙트 ABI
+    //private string contractABI = "";
+
+    //@notion 스마트 컨트랙트 주소
+    //private string contractAddress = "0xa77c33d38Fd80031787aa614b14aAB4EAced39DF";
+
+    //@notion 가나슈 네트워크 주소
+    //private string NetworkURL = "http://127.0.0.1:7545";
+
+    //@notion Web3인스턴스
+    //private Web3 web3;
+
+    //@notion 컨트랙트 인스턴스
+    //private Contract contract
+    
+    void Start()
+    {
+        //@notion Web3 인스턴스 생성
+        //web3 = new Web3(NetworkURL);
+
+        //ABI를 사용하여 컨트랙트 인스턴스 생성
+        //contract = new Contract(web3, contractABI, contractAddress);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
