b3d3a9ceef25c6a931f30f6dfe5017b98e8c6c9c
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-08-03 14:45:33 +0900

    nft

--- Assets/Scripts/Contract/PPC721Contract.cs
+++ Assets/Scripts/Contract/PPC721Contract.cs
@@ -1,80 +1,59 @@
 using UnityEngine;
-using System.Collections;
-using System.Numerics;
 using Nethereum.Web3;
+using System.Numerics;
 using Nethereum.Hex.HexTypes;
-using System.IO;
 using System;
+using System.Collections;
 
 
 public class PPC721Contract : MonoBehaviour
 {
-    private Nethereum.Contracts.Contract _contract;
-    private Web3 _web3;
-    public static string userAddress;
+    public SmartContractInteraction contractInstance;
 
     void Awake()
     {
-        userAddress = "0x176feB0F409cecFd3362CD4C10fF730814368EfE";
-        Initialize();
+        contractInstance = gameObject.AddComponent<SmartContractInteraction>();
     }
-
     public void Initialize()
     {
-        _web3 = new Web3("http://127.0.0.1:7545");
-        var contractAbiPath = "Assets/contracts/PPC721 Contract.json";
-        var contractAbi = ReadAbiFromFile(contractAbiPath);
-        Debug.Log(contractAbi);
-        _contract = _web3.Eth.GetContract(contractAbi, "0xAfdC75da5E1a8eAdb9B5B5c691A4Ed3fd04Fb829");
-    }
-
-    private string ReadAbiFromFile(string filePath)
-    {
-        try
-        {
-            string jsonContent = File.ReadAllText(filePath);
-            var jsonObject = Newtonsoft.Json.Linq.JObject.Parse(jsonContent);
-            var abiObject = jsonObject["abi"];
-            return abiObject.ToString();
-        }
-        catch (Exception ex)
-        {
-            Debug.LogError($"Error reading ABI from file: {ex.Message}");
-            return null;
-        }
-
+        contractInstance.Init("PPC721Contract.json");
     }
 
 
-
-    public IEnumerator MintNFTCoroutine()
+    
+    public IEnumerator MintNFT()
     {
-        string recipient = "0x176feB0F409cecFd3362CD4C10fF730814368EfE";
+
+
+        var function = this.contractInstance.contract.GetFunction("mintNFT");
+
+
+        // Transaction Input 생성
+        string recipient = "0xE503081665f268c99ff22F45Df5FC8f3A21Ef0C8";
         string tokenURI = "https://gateway.pinata.cloud/ipfs/QmP25FavCnPjQpuvM9noxxfZNNRKW6cmUYsEg3LwSJ22gm/1.json";
-        System.Numerics.BigInteger nftPrice = new System.Numerics.BigInteger(123);
-
-        var function = _contract.GetFunction("mintNFT");
-        var gas = new HexBigInteger(3000000);
+        System.Numerics.BigInteger nftPrice = new System.Numerics.BigInteger(1);
+        var data = function.GetData(recipient, tokenURI, nftPrice);
+        decimal ethValue = 0;
+        string senderAddress = "0xE503081665f268c99ff22F45Df5FC8f3A21Ef0C8";
 
         // 스마트 컨트랙트의 메서드에 전달할 트랜잭션 입력 생성
-        var transactionInput = function.CreateTransactionInput(PPC721Contract.userAddress, gas, gas, new object[] { recipient, tokenURI, nftPrice });
-        Debug.Log(transactionInput.From);
+        yield return FrequentlyUsed.SendTransaction(this.contractInstance, this.contractInstance.contractAddress, ethValue, data, (contractAddress, err) => {
+            if (contractAddress == null)
+            {
+                Debug.Log(err);
+            }
+            else
+            {
+
+                Debug.Log(contractAddress);
+            }
+        });
 
 
-        // SendTransactionAndWaitForReceiptAsync 메서드로 트랜잭션 전송 및 영수증 대기
-        var task = function.SendTransactionAsync(transactionInput);
-        yield return new WaitUntil(() => task.IsCompleted);
-        // 비동기 작업 완료 여부를 확인하며 대기
-        if (task.IsFaulted)
-        {
-            Debug.LogError(task.Exception);
-        }
-        else
-        {
-            Debug.Log("Transfer completed with transaction hash: " + task.Result);
-        }
+
     }
-
+   
+    /*
     // NFT 토큰 데이터 클래스 정의
     public class NftTokenData
     {
@@ -107,10 +86,11 @@ public class PPC721Contract : MonoBehaviour
             }
         }
     }
+    */
 
     private void Start()
     {
-        StartCoroutine(MintNFTCoroutine());
+        StartCoroutine(MintNFT());
         //StartCoroutine(GetNftTokenListCoroutine(userAddress));
     }
 }
